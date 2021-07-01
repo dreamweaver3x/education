6e28690b277362fc75ff0b363ddf5f9d0992cc3d
Владислав <dreamweaver3x@gmail.com>
2021-07-01 23:48:40 +0300

    education

--- 10/main.go
+++ 10/main.go
@@ -28,12 +28,25 @@ func main() {
 	}
 
 	mapka := make(map[int][]float64)
+	mapka2 := make(map[string][]float64)
 
 	for _, v := range floats {
 		mapka[(int(v)/10)*10] = append(mapka[(int(v)/10)*10], v)
+		if v > -10 && v < 0 {
+			mapka2["-0"] = append(mapka2["-0"], v)
+			continue
+		}
+		if v >= 0 && v < 10 {
+			mapka2["0"] = append(mapka2["0"], v)
+			continue
+		}
+		mapka2[string((int(v)/10)*10)] = append(mapka2[string((int(v)/10)*10)], v)
 	}
 
 	for k, v := range mapka {
 		fmt.Printf("key: %d, value: %v\n", k, v)
+	}
+	for k, v := range mapka2 {
+		fmt.Printf("key: %s, value: %v\n", k, v)
 	}
 }
